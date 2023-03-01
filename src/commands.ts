ad52f56eff5571e6af6005c17c6f76b1b84ec0d0
m4rch <m4rch3n1ng@gmail.com>
2023-03-01 19:37:39 +0100

    initial md

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,5 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js"
+import { EmbedBuilder, SlashCommandBuilder } from "@discordjs/builders"
+import type { ChatInputCommandInteraction } from "discord.js"
 import { rm } from "node:fs/promises"
 import collectMessages from "./collect/index.js"
 import { writeProgress, getTotalChannelCount } from "./collect/utils.js"
@@ -7,7 +8,9 @@ import { isMember, isThread, isChannel } from "./utils.js"
 let collectIsRunning = new Map<string, boolean>
 export const collect = {
 	name: "collect",
-	data: new SlashCommandBuilder().setName("collect").setDescription("collect all messages"),
+	data: new SlashCommandBuilder()
+		.setName("collect")
+		.setDescription("collect all messages"),
 	async execute ( interaction: ChatInputCommandInteraction ): Promise<any> {
 		if (!isMember(interaction) || !isChannel(interaction) || isThread(interaction)) return
 
@@ -26,5 +29,21 @@ export const collect = {
 
 		await rm(zipPath)
 		collectIsRunning.set(interaction.guild.id, false)
+	}
+}
+
+export const entry = {
+	name: "entry",
+	data: new SlashCommandBuilder()
+		.setName("entry")
+		.setDescription("collect all messages")
+		.addStringOption(( option ) => option.setName("word").setDescription("word or article id").setRequired(true)),
+	async execute ( interaction: ChatInputCommandInteraction ): Promise<any> {
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0x008080)
+			.setAuthor({ name: "blictionary", iconURL: "https://cdn.discordapp.com/icons/876853152702398505/6451755d09135e4ce8f96060cc0d4c49.webp" })
+			.setTimestamp()
+
+		interaction.reply({ embeds: [ exampleEmbed ]})
 	}
 }
