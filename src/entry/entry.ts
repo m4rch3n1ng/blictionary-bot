da0fb7d3c98917431d3b68e2b2ce47c14eda4fc2
m4rch <m4rch3n1ng@gmail.com>
2023-03-10 21:13:47 +0100

    add fuzzy search, setTitle

--- src/entry/entry.ts
+++ src/entry/entry.ts
@@ -2,15 +2,17 @@ import { APIEmbed, ChatInputCommandInteraction, EmbedBuilder } from "discord.js"
 import { join as joinPath } from "node:path"
 import { existsSync } from "node:fs"
 import { readdir, readFile } from "node:fs/promises"
-import { initMark } from "./markdown.js"
+import { initMark, wordClassToString } from "./markdown.js"
+import { fuzzy } from "./fuzzy.js"
 
 export interface Entry {
 	word: string
 	class: string | string[]
 	pronounciation: Pronounciation
-	forms?: string
-	etymology: string
+	forms?: string | string[]
+	etymology: string | string[]
 	definitions: (Definition | TopDefinition)[]
+	sub?: SubEntry[]
 }
 
 interface Pronounciation {
@@ -19,18 +21,25 @@ interface Pronounciation {
 	note?: string
 }
 
-export interface TopDefinition {
+interface TopDefinition {
 	text?: string
 	sub: Definition[]
 	quotes?: Quote[]
 }
 
-export interface Definition {
+interface Definition {
 	text: string
 	quotes?: Quote[]
 }
 
-export interface Quote {
+interface SubEntry {
+	word: string
+	class: string | string[]
+	definition: string
+	quotes?: Quote[]
+}
+
+interface Quote {
 	date: string
 	author: string
 	location: string
@@ -42,17 +51,57 @@ export async function makeEmbed ( interaction: ChatInputCommandInteraction ): Pr
 	const word = interaction.options.get("word")
 	if (!word) throw new Error("no option specified")
 
-	const value = word.value as string
+	const query = word.value as string
+	const allMeta = await fetchAllMeta()
 
 	// todo implement search
-	if (/^\d+$/.test(value)) {
-		const id = value
+	if (/^\d+$/.test(query)) {
+		const id = query
 		const entry = await getEntry(id)
 
-		return _makeEmbed(entry)
+		return _makeEmbed(allMeta, entry)
 	} else {
-		throw new Error("only ids currently supported")
+		const extraMeta = _extSmallMeta(allMeta)
+
+		const allFind = extraMeta.filter(({ word }) => word === query)
+		if (allFind.length === 1 && allFind[0]) {
+			const find = allFind[0]
+			if (find.sub) {
+				return _makeSubEmbed(allMeta, find)
+			} else {
+				const entry = await getEntry(find.id)
+				return _makeEmbed(allMeta, entry)
+			}
+		}
+
+		const filteredMeta = fuzzy(extraMeta, query)
+		const bestResult = filteredMeta[0]
+
+		if (!bestResult) throw new Error("no match")
+
+		if (bestResult.sub) {
+			return _makeSubEmbed(allMeta, bestResult)
+		} else {
+			const entry = await getEntry(bestResult.id)
+			return _makeEmbed(allMeta, entry)
+		}
 	}
+}
+
+export interface subSubEntry extends SubEntry {
+	sub: true
+}
+
+export interface subSmallMeta extends smallMeta {
+	sub: false
+}
+
+// todo comments lmao
+function _extSmallMeta ( meta: subMeta[] ): (subSmallMeta | subSubEntry)[] {
+	const allSmall: (smallMeta & { sub: false })[] = meta.map(( meta ) => ({ ...meta, sub: false }))
+	const allSub: (SubEntry & { sub: true })[] = (meta.map(({ sub }) => sub).filter(( sub ) => sub).flat() as SubEntry[]).map(( sub ) => ({ ...sub, sub: true }))
+
+	return [ ...allSmall, ...allSub ]
 }
 
 function _makePronounciation ( inp: Entry["pronounciation"] ) {
@@ -81,8 +130,22 @@ function _makeDefinitions ( inp: Entry["definitions"] ) {
 	return defs.join("\n")
 }
 
-async function _makeEmbed ( entry: Entry ): Promise<EmbedBuilder> {
-	const allMeta = await fetchAllMeta()
+async function _makeSubEmbed ( allMeta: smallMeta[], entry: SubEntry ) {
+	const marked = initMark(allMeta)
+
+	return new EmbedBuilder()
+		.setColor(0x008080)
+		.setAuthor({
+			name: "blictionary",
+			iconURL: "https://cdn.discordapp.com/icons/876853152702398505/6451755d09135e4ce8f96060cc0d4c49.webp",
+			url: process.env.BLICTIONARY_URL
+		})
+		.setTitle(`${entry.word}, ${wordClassToString(entry.class)}`)
+		.addFields({ name: "__Definition:__", value: marked(entry.definition) })
+		.setTimestamp()
+}
+
+async function _makeEmbed ( allMeta: smallMeta[], entry: Entry ): Promise<EmbedBuilder> {
 	const marked = initMark(allMeta)
 
 	const pronounciationTxt = _makePronounciation(entry.pronounciation)
@@ -95,6 +158,7 @@ async function _makeEmbed ( entry: Entry ): Promise<EmbedBuilder> {
 			iconURL: "https://cdn.discordapp.com/icons/876853152702398505/6451755d09135e4ce8f96060cc0d4c49.webp",
 			url: process.env.BLICTIONARY_URL
 		})
+		.setTitle(`${entry.word}, ${wordClassToString(entry.class)}`)
 		.addFields({ name: "__Pronounciation:__", value: marked(pronounciationTxt) })
 		.addFields({ name: "__Definitions:__", value: marked(definitionTxt) })
 		.setTimestamp()
@@ -115,10 +179,14 @@ export interface smallMeta {
 	class: string | string[],
 }
 
-export async function fetchAllMeta () {
+interface subMeta extends smallMeta {
+	sub?: SubEntry[]
+}
+
+export async function fetchAllMeta (): Promise<subMeta[]> {
 	const path = "entries"
 	const all = await readdir(path)
-	const allMeta: smallMeta[] = await Promise.all(
+	const allMeta: (subMeta)[] = await Promise.all(
 		all.filter(( fileName ) => /\.json$/.test(fileName)).map(async ( fileName ) => {
 			const filePath = joinPath(path, fileName)
 			const content = await readFile(filePath)
@@ -126,7 +194,8 @@ export async function fetchAllMeta () {
 			return {
 				id: fileName.slice(0, -5),
 				word: entry.word,
-				class: entry.class
+				class: entry.class,
+				sub: entry.sub
 			}
 		})
 	)
