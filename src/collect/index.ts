0deca1b6a4d343a84d7efc8c8583f965816e5891
may <m4rch3n1ng@gmail.com>
2023-04-14 02:45:28 +0200

    bump, fix typescript errors

--- src/collect/index.ts
+++ src/collect/index.ts
@@ -96,7 +96,7 @@ interface interCommand {
 }
 
 async function fetchAllArchivedThreads ( threadManager: ThreadManager, id?: AnyThreadChannel<boolean> ): Promise<Collection<string, AnyThreadChannel>> {
-	const { threads } = await threadManager.fetchArchived({ limit: 100, before: id })
+	const { threads } = id ? await threadManager.fetchArchived({ limit: 100, before: id }) : await threadManager.fetchArchived({ limit: 100 })
 
 	if (threads.size < 100) {
 		return threads
@@ -108,7 +108,7 @@ async function fetchAllArchivedThreads ( threadManager: ThreadManager, id?: AnyT
 }
 
 async function fetchAllMessages ( messageManager: MessageManager<true>, id?: string, name?: string ): Promise<(interMessage|interCommand)[]> {
-	const allCollection = await messageManager.fetch({ limit: 100, before: id })
+	const allCollection = id ? await messageManager.fetch({ limit: 100, before: id }) : await messageManager.fetch({ limit: 100 })
 
 	if (allCollection.size < 100) {
 		return format(allCollection)
